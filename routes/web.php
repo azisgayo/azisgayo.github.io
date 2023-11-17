71bc6d5e687e38bf71707c979e357277c2064b16
azisgayo <azis.abdul26@gmail.com>
2023-11-17 10:09:13 +0700

    menambahkan file home dan frofile

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('/', 'home');
+// Route::get('/', fn () => view('home'));
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('home');
+// });
+ 
+Route::get('profile', function () {
+    $name = "Aziz";
+    return view('profile',[
+        'name' => $name 
+    ]);
+
 });
+ 
